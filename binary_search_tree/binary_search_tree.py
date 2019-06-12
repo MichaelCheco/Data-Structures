74d1811a540b87a36daab76d0b6e326d08bfb138
Michael <checomichael2@gmail.com>
2019-06-12 17:17:40 -0400

    contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -4,13 +4,6 @@ class BinarySearchTree:
         self.left = None
         self.right = None
 
-# .     8
-    #   3 .    10 .        #11 .      7
-        #   3 .    10
-        #          9   11
-    # 1   6 .     14
-    #    4 . 7 . 13
-
     def print_tree(self):
         nodes = []
         left_nodes = []
@@ -64,7 +57,25 @@ class BinarySearchTree:
     #     # adds the input value to the binary search tree,
     #     # adhering to the rules of the ordering of elements in a binary search tree.
 
-    # def contains(self, target):
+      def contains(self, target):
+        if self.left is None and self.right is None:
+          return False
+        if target == self.value:
+          return True
+        if target > self.value:
+          if self.right:
+            self = self.right
+            return self.contains(target)
+          else:
+            return False
+        elif target < self.value:
+          if self.left:
+            self = self.left
+            return self.contains(target)
+          else:
+            return False  
+
+
     #     # searches the binary search tree for the input value,
     #     # returning a boolean indicating whether the value exists in the tree or not.
 
