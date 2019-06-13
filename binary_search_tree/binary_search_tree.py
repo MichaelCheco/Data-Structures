96f0dadc36c16710d25f74033e9655444cff8ad7
Michael <checomichael2@gmail.com>
2019-06-12 22:37:57 -0400

    heap insert

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -21,72 +21,55 @@ class BinarySearchTree:
         return "->".join(nodes)
 
     def insert(self, value):
-        node = BinarySearchTree(value)
-        if self.left is None and node.value < self.value:
-            self.left = node
-            return node
-        if self.right is None and node.value > self.value:
-            self.right = node
-            return node
-        nodes_to_visit = []
-        lower_bound = None
-        upper_bound = None
-        if node.value > self.value:
-            nodes_to_visit.append(self.right)
-            lower_bound = self.value
-            upper_bound = 999999
+        # check if the new node's value is less than our current node's value
+    if value < self.value:
+        # if there's no left child here already, place the new node here
+        if not self.left:
+            self.left = BinarySearchTree(new_tree)
         else:
-            nodes_to_visit.append(self.right)
-            lower_bound = -99999
-            upper_bound = self.value
-        while nodes_to_visit:
-            current_node = nodes_to_visit.pop()
-            if current_node.value > node.value and current_node.left is None:
-                current_node.left = node
-                # print(f"node placed at {current_node.value}")
-                break
-            elif current_node.value < node.value and current_node.right is None:
-                current_node.right = node
-                # print(f"node placed at {current_node.value}")
-                break
-
-            if current_node.value < node.value:
-                nodes_to_visit.append(current_node.right)
-                lower_bound = current_node.value
+            # otherwise, repeat the process!
+            self.left.insert(value)
+    # check if the new node's value is greater than or equal to our current node's value
+    elif value >= self.value:
+        # if there's no right child here already, place the new node here
+        if not self.right:
+            self.right = BinarySearchTree(new_tree)
+        else:
+            # otherwise, repeat the process!
+            self.right.insert(value)
 
     #     # adds the input value to the binary search tree,
     #     # adhering to the rules of the ordering of elements in a binary search tree.
 
-      def contains(self, target):
-        if self.left is None and self.right is None:
-          return False
-        if target == self.value:
-          return True
-        if target > self.value:
-          if self.right:
-            self = self.right
-            return self.contains(target)
-          else:
-            return False
-        elif target < self.value:
-          if self.left:
-            self = self.left
-            return self.contains(target)
-          else:
-            return False  
-
+        def contains(self, target):
+            if self.left is None and self.right is None:
+                return False
+            if target == self.value:
+                return True
+            if target > self.value:
+                if self.right:
+                    self = self.right
+                    return self.contains(target)
+                else:
+                    return False
+            elif target < self.value:
+                if self.left:
+                    self = self.left
+                    return self.contains(target)
+                else:
+                    return False
 
     #     # searches the binary search tree for the input value,
     #     # returning a boolean indicating whether the value exists in the tree or not.
 
     def get_max(self):
-        # returns the maximum value in the binary search tree.
-        # if possible go right if not return root
-        # traverse right until None is reached and then return
+            # returns the maximum value in the binary search tree.
+            # if possible go right if not return root
+            # traverse right until None is reached and then return
         if self.right is None:
-          return self.value
+            return self.value
         while self.right:
-          self = self.right
+            self = self.right
         return self.value
 
     def for_each(self, cb):
@@ -96,15 +79,14 @@ class BinarySearchTree:
         # perform cb on each value
         for_each_queue = [self]
         while for_each_queue:
-          item = for_each_queue.pop(0)
-          new_val = cb(item.value)
-          item.value = new_val
-          if item.left:
-            for_each_queue.append(item.left)
-          if item.right:
-            for_each_queue.append(item.right)
+            item = for_each_queue.pop(0)
+            new_val = cb(item.value)
+            item.value = new_val
+            if item.left:
+                for_each_queue.append(item.left)
+            if item.right:
+                for_each_queue.append(item.right)
     #     example cb for for_each method
     #     def add_5(val):
     #       val = val + 5
-    #       return val 
-
+    #       return val
--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,62 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def lemmesee(self):
+        x = [str(i) for i in self.storage]
+        print(x)
+        return "--> ".join(x)
 
-  def delete(self):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        if len(self.storage) == 1:
+            return
+        return self._bubble_up(len(self.storage) - 1)
 
-  def get_max(self):
-    pass
+    def _bubble_up(self, index):
+        while index > 0:
+            parent = (index - 1)
+            if self.storage[index] > self.storage[parent]:
+                    self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                    index = parent
+            else:
+                    break
 
-  def get_size(self):
-    pass
+      # insert adds the input value into the heap; this method should ensure that the
+      #  inserted value is in the correct spot in the heap
 
-  def _bubble_up(self, index):
-    pass
+      # PARENT(i) = i/2	Return the index of the father node
+      # LEFT(i) = 2i	Return the index of the left child
+      # RIGHT(i) = 2i+1	Return the index of the right child
 
-  def _sift_down(self, index):
-    pass
+    def delete(self):
+        pass
+
+    def get_max(self):
+        pass
+
+    def get_size(self):
+        pass
+
+    def _bubble_up(self, index):
+        # keep bubbling up until we've either reached the top of the heap
+        # or we've reached a point where the parent is higher prio
+        while index > 0:
+             # on a single bubble up iteration
+             # get the parent index
+            parent = (index - 1) // 2
+            # compare the child against the value of the parent
+            # if the child's value is higher prio than its parent's value
+               if self.storage[index] > self.storage[parent]:
+                      # swap them
+                    self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                     # update the child's index to be the new index it is now at
+                    index = parent
+             # otherwise, child is at a valid spot
+                else:
+                      # stop bubbling up
+                    break
+
+    def _sift_down(self, index):
+        pass
+
